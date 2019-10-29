c90aadb7860d560dd9e64c1459eeabb806542750
DuniaNdewon <duniandewon@gmail.com>
2019-10-29 10:40:15 +0700

    Add contact form to the home page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Contacts from '../contacts/Contacts';
+import ContactFrom from '../contacts/ContactFrom';
 
 const Home = () => {
   return (
     <div className='grid-2'>
-      <div>{/* Contact Form */}</div>
+      <div>
+        <ContactFrom />
+      </div>
       <div>
         <Contacts />
       </div>
