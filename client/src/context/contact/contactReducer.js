616f020bcbe97b4d2ef386e7ef608dd36cb030bf
DuniaNdewon <duniandewon@gmail.com>
2019-11-09 11:52:37 +0700

    Add delete contact functionality

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -7,12 +7,21 @@ import {
   FILTER_CONTACT,
   CLEAR_FILTER
 } from '../types';
+
 export default (state, action) => {
   switch (action.type) {
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [...state.contacts, action.payload]
+      };
+
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
       };
 
     default:
