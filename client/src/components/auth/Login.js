af5b1f828c31966210f26b205f5d80c9ae23c6b1
Mohammad Abolfaz <duniandewon@gmail.com>
2020-02-08 11:48:15 +0700

    Add user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Login = () => {
+const Login = props => {
+  const { login, error, clearErrors, isAuthenticated } = useContext(
+    AuthContext
+  );
+
+  const { setAlert } = useContext(AlertContext);
+
   const [User, setUser] = useState({
     email: '',
     password: ''
@@ -11,10 +19,25 @@ const Login = () => {
   const onChange = e => setUser({ ...User, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
-    e.preventDevault();
-
-    console.log('User Logged in');
+    e.preventDefault();
+    if (email === '') {
+      setAlert("Email shouldn't be emapty", 'danger');
+    } else if (password === '') {
+      setAlert("Password shouldn't be emapty", 'danger');
+    } else {
+      login({ email, password });
+    }
   };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+    if (error === 'invalid credentials') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+  }, [error, isAuthenticated, props.history]);
   return (
     <div className='form-container'>
       <h1>
