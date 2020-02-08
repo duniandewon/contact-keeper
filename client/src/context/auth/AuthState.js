9a228546c63ccda2f7a9e7b81f65afaf8317118b
Mohammad Abolfaz <duniandewon@gmail.com>
2020-02-08 13:39:03 +0700

    Add logout

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -90,7 +90,11 @@ const AuthState = props => {
   };
 
   /** logout */
-  const logout = () => console.log('ehllo');
+  const logout = () => {
+    dispatch({
+      type: LOGOUT
+    });
+  };
 
   /** clear errors */
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
